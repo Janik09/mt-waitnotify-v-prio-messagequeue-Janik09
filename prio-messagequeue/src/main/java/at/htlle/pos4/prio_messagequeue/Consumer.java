428c38963c2154f8a482381eca54e2791e8fea11
Janik09 <janikgierer@gmail.com>
2025-05-08 09:56:40 +0200

    Commit 8.05.2025

--- prio-messagequeue/src/main/java/at/htlle/pos4/prio_messagequeue/Consumer.java
+++ prio-messagequeue/src/main/java/at/htlle/pos4/prio_messagequeue/Consumer.java
@@ -7,20 +7,9 @@ public class Consumer extends Thread {
     private final PriorityMessageQueue queue;
     private final Random random = new Random();
 
-    public Consumer(String name, PriorityMessageQueue queue) {
-        super(name);
+
+    public Consumer(PriorityMessageQueue queue) {
         this.queue = queue;
     }
-
-    @Override
-    public void run() {
-        try {
-            while (true) {
-                queue.receiveMessage();
-                Thread.sleep(random.nextInt(1500) + 500);
-            }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-    }
 }
+
