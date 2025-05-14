d80b0d66327e4c4ef009fc33dd32f6db86da93ee
Janik09 <janikgierer@gmail.com>
2025-05-14 21:46:08 +0200

    Commit 14.05.2025

--- prio-messagequeue/src/main/java/at/htlle/pos4/prio_messagequeue/App.java
+++ prio-messagequeue/src/main/java/at/htlle/pos4/prio_messagequeue/App.java
@@ -1,11 +1,19 @@
 package at.htlle.pos4.prio_messagequeue;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
+        PriorityMessageQueue queue = new PriorityMessageQueue(5); // max 5 Nachrichten
 
+        // Producer starten
+        for (int i = 1; i <= 2; i++) {
+            Producer producer = new Producer("Producer" + i, queue);
+            producer.start();
+        }
+
+        // Consumer starten
+        for (int i = 1; i <= 2; i++) {
+            Consumer consumer = new Consumer("Consumer" + i, queue);
+            consumer.start();
+        }
     }
 }
--- prio-messagequeue/src/main/java/at/htlle/pos4/prio_messagequeue/Consumer.java
+++ prio-messagequeue/src/main/java/at/htlle/pos4/prio_messagequeue/Consumer.java
@@ -1,15 +1,26 @@
 package at.htlle.pos4.prio_messagequeue;
 
-
 import java.util.Random;
 
 public class Consumer extends Thread {
     private final PriorityMessageQueue queue;
     private final Random random = new Random();
 
-
-    public Consumer(PriorityMessageQueue queue) {
+    public Consumer(String name, PriorityMessageQueue queue) {
+        super(name);
         this.queue = queue;
     }
-}
 
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Message msg = queue.receiveMessage();
+                // Verarbeitung simulieren
+                Thread.sleep(random.nextInt(1000) + 500);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
